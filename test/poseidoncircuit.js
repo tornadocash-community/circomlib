afb4eff954c6a43f3b30c0792459416a26f1cf3f
poma <semenov.roma@gmail.com>
2020-10-27 13:23:41 +0300

    use 35 rounds for poseidon hash, update poseidon constants from sage script for 35 rounds

--- test/poseidoncircuit.js
+++ test/poseidoncircuit.js
@@ -23,7 +23,7 @@ describe("Poseidon Circuit test", function () {
 
     it("Should check constrain of hash([1, 2])", async () => {
         const hash = poseidon([1, 2]);
-        assert.equal("17117985411748610629288516079940078114952304104811071254131751175361957805920", hash.toString());
+        assert.equal("0x11ad302b36a2d7e09653c8e90618f00c06cd0a7348e52cdf2ccced3c3abec679", "0x" + hash.toString(16));
         const w = await circuit2.calculateWitness({inputs: [1, 2]}, true);
         const res = w[circuit2.getSignalIdx("main.out")];
         assert.equal(res.toString(), hash.toString());
@@ -32,7 +32,7 @@ describe("Poseidon Circuit test", function () {
 
     it("Should check constrain of hash([3, 4])", async () => {
         const hash = poseidon([3, 4]);
-        assert.equal("21867347236198497199818917118739170715216974132230970409806500217655788551452", hash.toString());
+        assert.equal("0x23939f0972e764d6e252060279aabaca8ec650ab30b17d2c13551bec2a66bcef", "0x" + hash.toString(16));
         const w = await circuit2.calculateWitness({inputs: [3, 4]});
         const res = w[circuit2.getSignalIdx("main.out")];
         assert.equal(res.toString(), hash.toString());
@@ -42,7 +42,7 @@ describe("Poseidon Circuit test", function () {
 
     it("Should check constrain of hash([1, 2, 3, 4])", async () => {
         const hash = poseidon([1, 2, 3, 4]);
-        assert.equal("10501812514110530158422365608831771203648472822841727510887411206067265790462", hash.toString());
+        assert.equal("0x2e4fb80ce74868b0d33f4acb22071d8d8f8da7d30ebf972e6e4f72a64bb0633f", "0x" + hash.toString(16));
         const w = await circuit4.calculateWitness({inputs: [1, 2, 3, 4]});
         const res = w[circuit4.getSignalIdx("main.out")];
         assert.equal(res.toString(), hash.toString());
@@ -51,7 +51,7 @@ describe("Poseidon Circuit test", function () {
 
     it("Should check constrain of hash([5, 6, 7, 8])", async () => {
         const hash = poseidon([5, 6, 7, 8]);
-        assert.equal("20761996991478317428195238015626872345373101531750069996451149877836620406299", hash.toString());
+        assert.equal("0x2a3fc67aa97766917ee06e927f35fd70f4655ad6c1f2e7bcd5c5c85aa3a8a974", "0x" + hash.toString(16));
         const w = await circuit4.calculateWitness({inputs: [5, 6, 7, 8]});
         const res = w[circuit4.getSignalIdx("main.out")];
         assert.equal(res.toString(), hash.toString());
